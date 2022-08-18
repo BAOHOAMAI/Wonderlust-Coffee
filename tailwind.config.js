885d166559a4ce6030eec4fc5430829fe98d2649
keisha <baohoa17112001@gmail.com>
2022-08-18 15:46:01 +0700

    upload code

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+    },
   },
   plugins: [],
 }
