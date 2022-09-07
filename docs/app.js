8f2dde4afca8e034c9a681efca6e3e5db367a501
keisha <baohoa17112001@gmail.com>
2022-09-07 14:44:37 +0700

    upload code

--- docs/app.js
+++ docs/app.js
@@ -1,3 +1,18 @@
+// Home Video  
+
+let btn = document.querySelector('.play-btn');
+let modal = document.querySelector('.overlay');
+let video = document.querySelector('video');
+let closeBtn = document.querySelector('.close-btn');
+
+btn.addEventListener('click', () => {
+    modal.classList.add('open');
+    video.currentTime= 0;
+});
+closeBtn.addEventListener('click', () => {
+    modal.classList.remove('open');
+})
+
 // Home Space
 var slides = document.querySelectorAll('.slider');
 var btns = document.querySelectorAll('.btn');
@@ -47,18 +62,3 @@ var repeat = function(activeClass) {
     repeater();
 }
 repeat();
-
-// Home Video  
-
-let btn = document.querySelector('.play-btn');
-let modal = document.querySelector('.overlay');
-let video = document.querySelector('video');
-let closeBtn = document.querySelector('.close-btn');
-
-btn.addEventListener('click', () => {
-    modal.classList.add('active');
-    video.currentTime= 0;
-});
-closeBtn.addEventListener('click', () => {
-    modal.classList.remove('active');
-})
